dbeac5b50ef2e30a6f6959e1b88e70cacc2b7f54
Ari Perez <ariperez@users.noreply.github.com>
2021-10-15 09:38:07 -0400

    feat(button): Add button component abstracted from chakra-ui button

---
+++ src/button/button.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Container, HStack, Stack } from "@chakra-ui/react"
+import { Button } from "./Button"
+
+
+export default {
+  title: "Button",
+  decorators: [
+    (Story: any) => (
+      <Container mt="40px">
+        <Story />
+      </Container>
+    ),
+  ],
+}
+
+export const basic = () => (
+  <>
+    <Button colorScheme="gray">Button</Button>
+    <Button colorScheme="red">Button</Button>
+    <Button colorScheme="green">Button</Button>
+    <Button colorScheme="blue">Button</Button>
+    <Button colorScheme="teal">Button</Button>
+    <Button colorScheme="pink">Button</Button>
+    <Button colorScheme="purple">Button</Button>
+    <Button colorScheme="cyan">Button</Button>
+    <Button colorScheme="orange">Button</Button>
+    <Button colorScheme="yellow">Button</Button>
+  </>
+)
