7d7beefad77ec9e1af3d0487efa906158894cf7c
Ari Perez <ariperez@users.noreply.github.com>
2021-11-18 17:26:20 +0000

    feat: chromatic viewports

--- src/sections/Heros/HomepageHero.stories.tsx
+++ src/sections/Heros/HomepageHero.stories.tsx
@@ -4,6 +4,9 @@ import { HomepageHero } from "./HomepageHero";
 export default {
   title: "Sections/Heros",
   component: HomepageHero,
+  parameters: {
+    chromatic: { viewports: [320, 1200] },
+  },
 } as ComponentMeta<typeof HomepageHero>;
 
 const Template: ComponentStory<typeof HomepageHero> = (args) => {
