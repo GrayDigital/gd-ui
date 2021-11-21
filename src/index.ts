6d79aefc83d9e61b6398d84b823a31566d7e70fa
Brandon Clapp <brandonclapp@outlook.com>
2021-11-20 19:15:30 -0600

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from "./sections/Brands/Brands";
 export * from "./sections/Header/Header";
 export * from "./sections/Heros/Hero";
 export * from "./sections/Heros/HomepageHero";
-export * from "./sections/Studies/Studies";
+export * from "./sections/Heros/Studies";
 export * from "./sections/Impact/Impact";
 export * from "./sections/SideBySide/SideBySide";
