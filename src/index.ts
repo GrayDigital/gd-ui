d38acadc161b17bd6fddf5bb9ae84b79ffc63e4d
Brandon Clapp <brandonclapp@outlook.com>
2021-11-20 21:57:15 -0600

    Remove impact component

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export * from "./sections/Header/Header";
 export * from "./sections/Heros/Hero";
 export * from "./sections/Heros/HomepageHero";
 export * from "./sections/Heros/Studies";
-export * from "./sections/Impact/Impact";
 export * from "./sections/SideBySide/SideBySide";
