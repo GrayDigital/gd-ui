05334a5a98bf1149a69cc768cdfd5dd4adcaac4c
Ari Perez <ariperez@users.noreply.github.com>
2021-11-25 12:49:06 -0400

    feat: export homepage hero

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./landing/Contact";
 export * from "./landing/Mission";
 export * from "./landing/Studies";
+export * from "./landing/HomepageHero";
 export * from "./shared/Header/Header";
 export * from "./shared/sections/Brands/Brands";
 export * from "./shared/sections/SideBySide/SideBySide";
